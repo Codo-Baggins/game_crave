2240498403aedadba8ba5f595ee69ee957c847c0
Joe Lopez <lopez.4.joseph@gmail.com>
2021-01-13 17:58:53 -0700

    Build out structure of individual components

--- src/App/App.js
+++ src/App/App.js
@@ -1,6 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import "./GameDetails.scss";
 
-const App =  
+const App = () => {
+  const [searchedGame, setSearchedGame] = useState(null);
+  const [wishList, setWishList] = useState([]);
+
+  return (
+    <main>
+      <section>
+        <h1>GAME CRAVE</h1>
+        <button>Home</button>
+      </section>
+    </main>
+  );
+};
 
 export default App;
---
+++ src/GameCard/GameCard.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "./GameDetails.scss";
+
+const GameCard = () => {
+  return (
+    <section>
+      <p>Title</p>
+      <img alt='Cover Art'>Some Image</img>
+    </section>
+  );
+};
+
+export default GameCard;
