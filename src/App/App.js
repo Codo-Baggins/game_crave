15f14c1e6df4717ba323fa73b08235edf65b13cc
Joe Lopez <lopez.4.joseph@gmail.com>
2021-01-13 18:38:05 -0700

    Create searchGame function

--- src/App/App.js
+++ src/App/App.js
@@ -5,11 +5,21 @@ const App = () => {
   const [searchedGame, setSearchedGame] = useState(null);
   const [wishList, setWishList] = useState([]);
 
+  const searchGame = (gameName) => {
+    setSearchedGame(gameName);
+  };
+
+  useEffect(() => searchGame(), [searchedGame]);
+
   return (
     <main>
       <section>
         <h1>GAME CRAVE</h1>
         <button>Home</button>
+        <form>
+          <input placeholder='Type The Title Of A Game'></input>
+          <button onClick={searchGame}>Search For A Game</button>
+        </form>
       </section>
     </main>
   );
