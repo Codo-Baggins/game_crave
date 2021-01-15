7860b134cf9ebc70be116a51bfa760db381c9828
Joe Lopez <lopez.4.joseph@gmail.com>
2021-01-14 20:16:24 -0700

    Allow ability to call api for different games

--- src/App/App.js
+++ src/App/App.js
@@ -1,15 +1,25 @@
 import React, { useState, useEffect } from "react";
-import "./GameDetails.scss";
+import "./App.scss";
+import { fetchGameData } from "../apiCalls";
+const REACT_APP_KEY = process.env.REACT_APP_RAWG_API_KEY;
 
 const App = () => {
-  const [searchedGame, setSearchedGame] = useState(null);
-  const [wishList, setWishList] = useState([]);
+  const [searchedGame, setSearchedGame] = useState("");
+  // const [wishList, setWishList] = useState([]);
+  const inputField = document.getElementById("search-form");
+  // const setGameState = (gameName) => {
+  //   setSearchedGame(gameName);
+  // };
 
   const searchGame = (gameName) => {
-    setSearchedGame(gameName);
+    if (searchedGame.length >= 0) {
+      fetchGameData(gameName, REACT_APP_KEY)
+        .then((data) => console.log(data))
+        .catch((error) => console.error);
+    }
   };
 
-  useEffect(() => searchGame(), [searchedGame]);
+  useEffect(() => {});
 
   return (
     <main>
@@ -17,8 +27,21 @@ const App = () => {
         <h1>GAME CRAVE</h1>
         <button>Home</button>
         <form>
-          <input placeholder='Type The Title Of A Game'></input>
-          <button onClick={searchGame}>Search For A Game</button>
+          <input
+            id='search-form'
+            placeholder='Type The Title Of A Game'
+            onChange={(event) => setSearchedGame(event.target.value)}
+          />
+
+          <button
+            value={searchedGame}
+            onClick={(event) => {
+              event.preventDefault();
+              searchGame(searchedGame);
+              //setSearchedGame(event.target.value)
+            }}>
+            Search For A Game
+          </button>
         </form>
       </section>
     </main>
