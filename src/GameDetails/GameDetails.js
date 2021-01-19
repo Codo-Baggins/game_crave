ffb807ef99c19bff0e7439a5a88ee0c0d476659f
Joe Lopez <lopez.4.joseph@gmail.com>
2021-01-18 17:43:27 -0700

    Stylings for the game details page

--- src/GameDetails/GameDetails.js
+++ src/GameDetails/GameDetails.js
@@ -13,26 +13,30 @@ const GameDetails = (props) => {
           backgroundImage: `url(${currentGameInfo.backgroundImage})`,
         }}>
         <section>
-          <p id='title'>{currentGameInfo.name}</p>
+          <p id='game-title'>{currentGameInfo.name}</p>
         </section>
-        <section>
+        <section id='main-info'>
           {currentGameInfo.stores.map((store) => {
             return (
-              <p>
-                {store.whereToBuy} -{" "}
-                <a href={store.storeURL}>{store.storeURL}</a>
-              </p>
+              <section className='stores'>
+                <h3>Where To Purchase</h3>
+                <h4 className='stores'>
+                  {store.whereToBuy} - <h5>{store.storeURL}</h5>
+                </h4>
+              </section>
             );
           })}
-          <p>{currentGameInfo.description}</p>
+          <div id='binding'>
+            <p id='game-description'>{currentGameInfo.description}</p>
+            <button
+              id='toggle-game-button'
+              onClick={() => {
+                props.toggleFromWishList();
+              }}>
+              {props.toggleButtonText()}
+            </button>
+          </div>
         </section>
-        <button
-          id='toggle-game-button'
-          onClick={() => {
-            props.toggleFromWishList();
-          }}>
-          {props.toggleButtonText()}
-        </button>
       </section>
     </main>
   );
